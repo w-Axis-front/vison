84cb93902f120500d48fd5e04f45de5524cdbb69
fedorova_kateryna <K_phedorova@ukr.net>
2021-03-11 10:27:13 +0200

    chore(main): delete comments

--- src/js/modules/animateLine.js
+++ src/js/modules/animateLine.js
@@ -1,23 +1,9 @@
 import marquee from "jquery.marquee";
 
 export default function animateLine() {
-    window.onload = function () {
-        const oQuotes = document.querySelector('.main__line-wrapper');
-        $('.main__line-wrapper').marquee({ duplicated: true, startVisible: true, speed: 60 });
+    const marquee = $('.main__line-wrapper');
 
-        /* Double content: */
-        // function fDoublingQuotes() {
-            // let nElem = oQuotes.children.length;
-            //
-            // setInterval(function() {
-            //     oQuotes.appendChild(oQuotes.children[0].cloneNode(true));
-            // }, 2500);
-            // for (let i = 0; i < nElem; i++) {
-            //     oQuotes.appendChild(oQuotes.children[i].cloneNode(true));
-            // }
-            // oQuotes.style.animationDuration = '3s,' + (nElem * 2) + 's';
-        // }
-
-        // fDoublingQuotes();
+    if (marquee.length > 0) {
+        marquee.marquee({duplicated: true, startVisible: true, speed: 60});
     }
 }
--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -44,15 +44,10 @@ export default function scroll() {
     // Add smooth scrolling to all links:
     if ($("a").length > 0) {
         $("a").on('click', function (event) {
-            // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
-                // Prevent default anchor click behavior
                 event.preventDefault();
-                // Store hash
                 let hash = this.hash;
 
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
                 }, 800, function () {
